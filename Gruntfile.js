8058dd958305d5f64a423a1da90897fa10633621
Muhammad Haroon <m_haroon96@hotmail.com>
2022-05-17 17:08:39 -0700

    added favicons

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
           files: [{
             expand: true,
             cwd: 'src',
-            src: ['img/**/*', 'css/**/*', '**/*.css'],
+            src: ['img/**/*', 'css/**/*', '**/*.css', 'favicon/**/*'],
             dest: 'build/'
           }]
         }
